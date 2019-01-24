f704f92d8c0a8393cf7c8edd6fd99415a9c3e401
robin-nilsson <robin@robins.nu>
2019-01-24 22:27:44 +0100

    WIP: Make use of new components in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import Button from '@material-ui/core/Button';
+import WeatherForm from './components/WeatherForm';
+import WeatherCard from './components/WeatherCard';
 import './App.scss';
 
 class App extends Component {
   state = {
     fetching: false,
     res: {},
+    showWeatherCard: false
   }
 
   fetchData = async () => {
@@ -23,16 +26,34 @@ class App extends Component {
     }, 500);
   }
 
+  onSubmit = (userInput) => {
+    this.setState({fetching: true})
+    this.setState({showWeatherCard: true})
+    this.getWeatherData(userInput);
+  }
+
+  getWeatherData = (userInput) => {
+    // Fake latency from server
+    setTimeout(() => {
+      this.setState({
+        fetching: false
+      })
+    }, 1000);
+  }
+
   render() {
-    const {fetching} = this.state
+    const {fetching, showWeatherCard} = this.state
     return (
       <div className="App">
         <header className="App-header">
-          <textarea readOnly value={JSON.stringify(this.state.res, null, 4)} style={{width: 500}} rows="5"></textarea>
+          {/* <textarea readOnly value={JSON.stringify(this.state.res, null, 4)} style={{width: 500}} rows="5"></textarea>
           <br />
           <Button variant="contained" color="primary" onClick={this.fetchData} disabled={fetching}>
             {fetching ? 'Fetching...' : 'Fetch from backend'}
-          </Button>
+          </Button> */}
+          <br />
+          <WeatherForm onSubmit={this.onSubmit} />
+          {showWeatherCard && <WeatherCard data={this.state} />}
         </header>
       </div>
     );
